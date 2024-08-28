1b520532224952b8581a605cfb1abdca495e901a
Abdul Wahab <abdulwahabee5@gmail.com>
2024-08-28 10:30:53 +0500

    Initial commit

--- backend/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/inotebook";
-
-const connectToMongo = async ()=>{
-    await mongoose.connect(mongoURI);
-    console.log('MongoDB connected successfully');
-}
-
-module.exports = connectToMongo;
